0e8ad112974beaf1487ce280b11b3547a4678cc0
AlbinoMartínez <mara8805@gmail.com>
2023-06-01 21:12:10 -0600

    Initial loading

---
+++ minifica.api/Controllers/BaseController.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using minifica.domain.Helpers;
+using minifica.domain.IManager;
+
+namespace minifica.api.Controllers
+{
+	public class BaseController<VM, E> : Controller where VM : class where E : class
+	{
+		private readonly IBaseManager<VM, E> _manager;
+
+		public BaseController(IBaseManager<VM, E> manager)
+		{
+			_manager = manager;
+		}
+
+		public virtual IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public virtual IActionResult GetAll()
+		{
+			var result = new JsonMessageResult();
+
+			try
+			{
+				result.Success = 1;
+				result.Detail = new { Models = _manager.GetAll() };
+			}
+			catch (Exception ex)
+			{
+				result.Success = 0;
+				result.Detail = new { Error = ex.Message.ToString() };
+			}
+
+			return Json(result);
+		}
+
+		[HttpGet]
+		public virtual IActionResult GetById(int id)
+		{
+			var result = new JsonMessageResult();
+
+			try
+			{
+				result.Success = 1;
+				result.Detail = new { Model = _manager.GetById(id) };
+			}
+			catch (Exception ex)
+			{
+				result.Success = 0;
+				result.Detail = new { Error = ex.Message };
+			}
+
+			return Json(result);
+		}
+
+		public virtual IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public virtual IActionResult Add(VM viewModel)
+		{
+			var result = new JsonMessageResult();
+
+			try
+			{
+				_manager.Add(viewModel);
+
+				result.Success = 1;
+				result.Detail = new { Message = "El registro se creó exitosamente!" };
+			}
+			catch (Exception ex)
+			{
+				result.Success = 0;
+				result.Detail = new { Error = ex.Message };
+			}
+
+			return Json(result);
+		}
+
+		[HttpPatch]
+		public virtual IActionResult Update(int id, VM viewModel)
+		{
+			var result = new JsonMessageResult();
+
+			try
+			{
+				_manager.Update(id, viewModel);
+
+				result.Success = 1;
+				result.Detail = new { Message = "El registro fue actualizado!" };
+			}
+			catch (Exception ex)
+			{
+				result.Success = 0;
+				result.Detail = new { Error = ex.Message };
+			}
+
+			return Json(result);
+		}
+
+
+		[HttpDelete]
+		public virtual IActionResult Delete(int id)
+		{
+			var result = new JsonMessageResult();
+
+			try
+			{
+				_manager.Delete(id);
+
+				result.Success = 1;
+				result.Detail = new { Message = "El registro fue eliminado!" };
+			}
+			catch (Exception ex)
+			{
+				result.Success = 0;
+				result.Detail = new { Error = ex.Message };
+			}
+
+			return Json(result);
+		}
+	}
+}
---
+++ minifica.domain/Helpers/Helpers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minifica.domain.Helpers
+{
+	public class JsonMessageResult
+	{
+		public int Success { get; set; }
+		public object Detail { get; set; }
+	}
+}
