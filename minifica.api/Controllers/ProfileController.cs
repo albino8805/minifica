ebd54175fb791c4f980982325d2f3fb5d5d46cc2
ferretisDev <ferretis.dev@outlook.com>
2023-06-05 17:22:48 -0600

    Created CRUD entity Profile

---
+++ minifica.api/Controllers/ProfileController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using minifica.data.Models;
+using minifica.data.ViewModels;
+using minifica.domain.IManager;
+
+namespace minifica.api.Controllers
+{
+    public class ProfileController : BaseController<ProfileViewModel, Profile>
+    {
+        public ProfileController(IProfileManager manager) : base(manager) { }
+
+        [HttpGet]
+        public override IActionResult GetAll() => base.GetAll();
+
+        [HttpGet]
+        public override IActionResult GetById(int id) => base.GetById(id);
+
+        [HttpPost]
+        public override IActionResult Add(ProfileViewModel profile) => base.Add(profile);
+
+        [HttpPatch]
+        public override IActionResult Update(int id, ProfileViewModel profile) => base.Update(id, profile);
+
+        [HttpDelete]
+        public override IActionResult Delete(int id) => base.Delete(id);
+    }
+}
---
+++ minifica.domain/Manager/ProfileManager.cs
@@ -0,0 +1,58 @@
+﻿using minifica.data.IRepository;
+using minifica.data.Models;
+using minifica.data.ViewModels;
+using minifica.domain.IManager;
+
+namespace minifica.domain.Manager
+{
+    internal class ProfileManager : BaseManager<ProfileViewModel, Profile>, IProfileManager
+    {
+        public ProfileManager(IProfileRepository repository) : base(repository) { }
+
+        public override Profile AddConverter(ProfileViewModel viewModel)
+        {
+            return new Profile()
+            {
+                Id = viewModel.Id,
+                Name = viewModel.Name,
+                Active = viewModel.Active,
+                CreatedAt = viewModel.CreatedAt,
+                UpdatedAt = viewModel.UpdatedAt
+            };
+        }
+
+        public override List<ProfileViewModel> CollectionConverter(List<Profile> entities)
+        {
+            return entities.Select(m => new ProfileViewModel()
+            {
+                Id = m.Id,
+                Name = m.Name,
+                Active = m.Active,
+                CreatedAt= m.CreatedAt,
+                UpdatedAt = m.UpdatedAt,
+            }).ToList();
+        }
+
+        public override Profile UpdatedConverter(ProfileViewModel viewModel, Profile entity)
+        {
+            entity.Name = viewModel.Name;
+            entity.Active = viewModel.Active;
+            entity.CreatedAt = viewModel.CreatedAt;
+            entity.UpdatedAt = viewModel.UpdatedAt;
+
+            return entity;
+        }
+
+        public override ProfileViewModel SingleConverter(Profile entity)
+        {
+            return new ProfileViewModel()
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+                Active = entity.Active,
+                CreatedAt = entity.CreatedAt,
+                UpdatedAt = entity.UpdatedAt
+            };
+        }
+    }
+}
