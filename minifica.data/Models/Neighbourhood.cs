3fd433c301b0654fa026608ad6613a6ba41595c0
angelmike83 <angel.mike@gmail.com>
2023-06-09 19:02:55 -0600

    Create entity Town and Neighbourhood

---
+++ minifica.data/Models/Neighbourhood.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minifica.data.Models
+{
+    public class Neighbourhood : BaseEntity
+    {
+        [Required]
+        [MaxLength(150)]
+        public string Name { get; set; }
+
+        [Required]
+        [ForeignKey("TownFKNavigation")]
+        public int TownFK { get; set; }
+
+        public virtual Town TownFKNavigation { get; set; } = null!;
+    }
+}
