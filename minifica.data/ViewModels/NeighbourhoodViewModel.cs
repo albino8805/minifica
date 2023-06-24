dccbf34471d173706a02a56d577c184b91708351
Miguel Angel <angelmike83@gmail.com>
2023-06-24 08:55:51 -0700

    Created CRUD entity Neighbourhood

---
+++ minifica.data/ViewModels/NeighbourhoodViewModel.cs
@@ -0,0 +1,16 @@
+﻿using minifica.data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minifica.data.ViewModels
+{
+    public class NeighbourhoodViewModel : BaseViewModel
+    {
+        public string Name { get; set; }
+
+        public int TownFk { get; set; }
+    }
+}
---
+++ minifica.domain/IManager/INeighbourhoodManager.cs
@@ -0,0 +1,13 @@
+﻿using minifica.data.Models;
+using minifica.data.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minifica.domain.IManager
+{
+    public interface INeighbourhoodManager : IBaseManager<NeighbourhoodViewModel, Neighbourhood>
+    {
+    }
+}
